5f54ed4fd41b9cfc44237dbd17fb0ef5ef87469e
Mikotwa <mikotwa@pm.me>
2022-01-15 21:30:48 +0800

    Preliminary trial for hooking getConnectionInfo

--- app/src/main/java/fuck/location/xposed/WLANHooker.kt
+++ app/src/main/java/fuck/location/xposed/WLANHooker.kt
@@ -1,4 +1,25 @@
 package fuck.location.xposed
 
+import android.annotation.SuppressLint
+import android.net.wifi.WifiInfo
+import android.os.Build
+import com.github.kyuubiran.ezxhelper.utils.findAllMethods
+import com.github.kyuubiran.ezxhelper.utils.hookMethod
+import com.github.kyuubiran.ezxhelper.utils.isPublic
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
 class WLANHooker {
+    //TODO: migrate to newer ConnectivityManager on S (deprecated)
+    @SuppressLint("PrivateApi")
+    fun HookWifiManager(lpparam: XC_LoadPackage.LoadPackageParam) {
+        val clazz: Class<*> = lpparam.classLoader.loadClass("com.android.server.wifi.WifiServiceImpl")
+        findAllMethods(clazz) {
+            name == "getConnectionInfo" && isPublic
+        }.hookMethod {
+            after { param ->
+                XposedBridge.log("in getConnectionInfo! caller: " + param.args[0])
+            }
+        }
+    }
 }
