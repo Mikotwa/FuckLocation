bc2c2de0c3dd5bd539d6aa9878a1a7d18d350ced
Mikotwa <mikotwa@pm.me>
2022-01-21 18:16:18 +0800

    Add com.android.phone to xposed scope & minor fixes

--- app/src/main/java/fuck/location/xposed/CellarHooker.kt
+++ app/src/main/java/fuck/location/xposed/CellarHooker.kt
@@ -1,4 +1,18 @@
 package fuck.location.xposed
 
-class CellarHooker {
+import com.github.kyuubiran.ezxhelper.utils.findAllMethods
+import com.github.kyuubiran.ezxhelper.utils.hookMethod
+import com.github.kyuubiran.ezxhelper.utils.isPrivate
+import com.github.kyuubiran.ezxhelper.utils.isStatic
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class CellarHooker(){
+    fun HookPhoneInterfaceManager(lpparam: XC_LoadPackage.LoadPackageParam) {
+        val clazz: Class<*> = lpparam.classLoader.loadClass("com.android.phone.PhoneInterfaceManager")
+        findAllMethods(clazz) {
+            name == "loadClassFromLoader" && isPrivate && isStatic
+        }.hookMethod {
+
+        }
+    }
 }
