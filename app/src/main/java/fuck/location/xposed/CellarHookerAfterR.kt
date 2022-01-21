82c32467101596ffc6b36a1588fe2f63515357ec
Mikotwa <mikotwa@pm.me>
2022-01-21 18:22:04 +0800

    Stub implemention in cellar location for testing prupose

--- app/src/main/java/fuck/location/xposed/CellarHookerAfterR.kt
+++ app/src/main/java/fuck/location/xposed/CellarHookerAfterR.kt
@@ -1,18 +1,18 @@
 package fuck.location.xposed
 
-import com.github.kyuubiran.ezxhelper.utils.findAllMethods
-import com.github.kyuubiran.ezxhelper.utils.hookMethod
-import com.github.kyuubiran.ezxhelper.utils.isPrivate
-import com.github.kyuubiran.ezxhelper.utils.isStatic
+import com.github.kyuubiran.ezxhelper.utils.*
+import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
-class CellarHooker(){
+class CellarHookerAfterR(){
     fun HookPhoneInterfaceManager(lpparam: XC_LoadPackage.LoadPackageParam) {
         val clazz: Class<*> = lpparam.classLoader.loadClass("com.android.phone.PhoneInterfaceManager")
         findAllMethods(clazz) {
-            name == "loadClassFromLoader" && isPrivate && isStatic
+            name == "getCellLocation" && isPublic
         }.hookMethod {
-
+            after { param ->
+                XposedBridge.log("FL: in getCellLocation! Caller package name: " + param.args[0])
+            }
         }
     }
 }
