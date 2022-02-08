5a568c9c9bc392304b4f15f11b778aca6728be1a
Mikotwa <mikotwa@pm.me>
2022-02-08 11:41:56 +0800

    Prepare for custom cellinfo callback result

--- app/src/main/java/fuck/location/xposed/cellar/TelephonyRegistryHooker.kt
+++ app/src/main/java/fuck/location/xposed/cellar/TelephonyRegistryHooker.kt
@@ -1,10 +1,11 @@
 package fuck.location.xposed.cellar
 
 import android.annotation.SuppressLint
+import android.os.Build
+import android.telephony.PhoneStateListener
+import android.telephony.TelephonyCallback
+import com.github.kyuubiran.ezxhelper.utils.*
 import com.github.kyuubiran.ezxhelper.utils.findAllMethods
-import com.github.kyuubiran.ezxhelper.utils.hookBefore
-import com.github.kyuubiran.ezxhelper.utils.hookMethod
-import com.github.kyuubiran.ezxhelper.utils.isPrivate
 import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 import fuck.location.xposed.helpers.ConfigGateway
@@ -19,14 +20,35 @@ class TelephonyRegistryHooker {
 
         findAllMethods(clazz) {
             name == "listen" && isPrivate
-        }.hookMethod {
-            before { param ->
-                val packageName = param.args[0] as String
+        }.hookBefore { param ->
+            val packageName = param.args[0] as String
 
-                XposedBridge.log("FL: [Cellar] in listen! Caller package name: $packageName")
-                if (ConfigGateway.get().inWhitelist(packageName)) {
-                    XposedBridge.log("FL: [Cellar] in whiteList! Dropping listen request...")
-                    param.result = null
+            XposedBridge.log("FL: [Cellar] in listen! Caller package name: $packageName")
+            if (ConfigGateway.get().inWhitelist(packageName)) {
+                XposedBridge.log("FL: [Cellar] in whiteList! Do not notify now for returning custom info.")
+
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+                    param.args[3] = false
+                } else {
+                    param.args[4] = false
+                }
+            }
+        }
+
+        findAllMethods(clazz) {
+            name == "notifyCellInfoForSubscriber" && isPublic
+        }.hookBefore {
+            XposedBridge.log("FL: [Cellar] in notifyCellInfoForSubscriber!")
+
+            val mRecords = findField(clazz) {
+                name == "mRecords"
+            }.get(clazz) as ArrayList<*>
+
+            mRecords.forEach { record ->
+                findAllFields(record.javaClass) {
+                    true
+                }.forEach { field ->
+                    XposedBridge.log("FL: [Cellar] $field")
                 }
             }
         }
