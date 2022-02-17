5b27cdebdf9d4f0f6ae94197141149af3fbcb590
Mikotwa <mikotwa@pm.me>
2022-02-17 15:49:27 +0800

    Add support for Nr network type

--- app/src/main/java/fuck/location/xposed/cellar/TelephonyRegistryHooker.kt
+++ app/src/main/java/fuck/location/xposed/cellar/TelephonyRegistryHooker.kt
@@ -3,7 +3,6 @@ package fuck.location.xposed.cellar
 import android.annotation.SuppressLint
 import android.os.Build
 import android.telephony.*
-import androidx.annotation.RequiresApi
 import com.github.kyuubiran.ezxhelper.utils.*
 import com.github.kyuubiran.ezxhelper.utils.findAllMethods
 import de.robv.android.xposed.XposedBridge
@@ -14,7 +13,6 @@ import fuck.location.xposed.helpers.ConfigGateway
 
 class TelephonyRegistryHooker {
     @ExperimentalStdlibApi
-    @RequiresApi(Build.VERSION_CODES.R)
     @SuppressLint("PrivateApi")
     fun hookListen(lpparam: XC_LoadPackage.LoadPackageParam) {
         val clazz: Class<*> =
@@ -57,21 +55,45 @@ class TelephonyRegistryHooker {
                                 if ((phoneId as Int) >= 0 && phoneId < (mCellIdentity as Array<*>).size) {
                                     val originalCellIdentity = mCellIdentity[phoneId]
                                     if (originalCellIdentity != null) {
-                                        when (originalCellIdentity) {
-                                            is CellIdentityLte -> {
-                                                findMethod(callBack.javaClass) {
-                                                    name == "onCellLocationChanged"
-                                                }.invoke(callBack, Lte().alterCellIdentity(originalCellIdentity))    // return cellIdentity
+                                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                                            when (originalCellIdentity) {
+                                                is CellIdentityLte -> {
+                                                    findMethod(callBack.javaClass) {
+                                                        name == "onCellLocationChanged"
+                                                    }.invoke(
+                                                        callBack,
+                                                        Lte().alterCellIdentity(originalCellIdentity)
+                                                    )
+                                                }
+                                                is CellIdentityNr -> {
+                                                    findMethod(callBack.javaClass) {
+                                                        name == "onCellLocationChanged"
+                                                    }.invoke(
+                                                        callBack,
+                                                        Nr().alterCellIdentity(originalCellIdentity)
+                                                    )
+                                                }
+                                                else -> {
+                                                    findMethod(callBack.javaClass) {
+                                                        name == "onCellLocationChanged"
+                                                    }.invoke(callBack, null)
+                                                }
                                             }
-                                            is CellIdentityNr -> {
-                                                findMethod(callBack.javaClass) {
-                                                    name == "onCellLocationChanged"
-                                                }.invoke(callBack, Nr().alterCellIdentity(originalCellIdentity))
-                                            }
-                                            else -> {
-                                                findMethod(callBack.javaClass) {
-                                                    name == "onCellLocationChanged"
-                                                }.invoke(callBack, null)
+                                        } else {    // Android 9 do not support 5G network
+                                            when (originalCellIdentity) {
+                                                is CellIdentityLte -> {
+                                                    findMethod(callBack.javaClass) {
+                                                        name == "onCellLocationChanged"
+                                                    }.invoke(
+                                                        callBack,
+                                                        Lte().alterCellIdentity(originalCellIdentity)
+                                                    )
+                                                }
+                                                else -> {
+                                                    findMethod(callBack.javaClass) {
+                                                        name == "onCellLocationChanged"
+                                                    }.invoke(callBack, null)
+                                                }
                                             }
                                         }
                                     } else {
@@ -85,13 +107,66 @@ class TelephonyRegistryHooker {
 
                         param.result = false
                     }
-
-                    // TODO: Maybe a better implement?
+                    
                     11 -> {
                         XposedBridge.log("FL: [Cellar] in whiteList! Alter EVENT_CELL_INFO_CHANGED for now.")
-                        findMethod(callBack.javaClass) {
-                            name == "onCellInfoChanged"
-                        }.invoke(callBack, null)    // return cellInfo
+
+                        if (phoneId != null) {
+                            val mCellInfo = findField(param.thisObject.javaClass) {
+                                name == "mCellInfo"
+                            }.get(param.thisObject)
+
+                            if (mCellInfo != null) {
+                                if ((phoneId as Int) >= 0 && phoneId < (mCellInfo as Array<*>).size) {
+                                    val originalCellInfoList = mCellInfo[phoneId]
+                                    if (originalCellInfoList != null) {
+                                        val modifiedCellInfoList = mutableListOf<CellInfo>()
+
+                                        (originalCellInfoList as List<*>).forEach { cellInfo ->
+                                            if (cellInfo != null) {
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                                                    when (cellInfo) {
+                                                        is CellInfoLte -> {
+                                                            modifiedCellInfoList.add(
+                                                                fuck.location.xposed.cellar.info.Lte()
+                                                                    .constructNewCellInfoLte(cellInfo)
+                                                            )
+                                                        }
+                                                        is CellInfoNr -> {
+                                                            modifiedCellInfoList.add(
+                                                                fuck.location.xposed.cellar.info.Nr()
+                                                                    .constructNewCellInfoNr(cellInfo)
+                                                            )
+                                                        }
+                                                    }
+                                                } else {
+                                                    when (cellInfo) {
+                                                        is CellInfoLte -> {
+                                                            modifiedCellInfoList.add(
+                                                                fuck.location.xposed.cellar.info.Lte()
+                                                                    .constructNewCellInfoLte(cellInfo)
+                                                            )
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+
+                                        findMethod(callBack.javaClass) {
+                                            name == "onCellInfoChanged"
+                                        }.invoke(callBack, modifiedCellInfoList)    // return cellInfo
+                                    } else {
+                                        findMethod(callBack.javaClass) {
+                                            name == "onCellInfoChanged"
+                                        }.invoke(callBack, null)
+                                    }
+                                }
+                            } else {
+                                findMethod(callBack.javaClass) {
+                                    name == "onCellInfoChanged"
+                                }.invoke(callBack, null)
+                            }
+                        }
 
                         param.result = false
                     }
--- app/src/main/java/fuck/location/xposed/cellar/info/Lte.kt
+++ app/src/main/java/fuck/location/xposed/cellar/info/Lte.kt
@@ -1,18 +1,12 @@
 package fuck.location.xposed.cellar.info
 
-import android.os.Build
 import android.telephony.CellIdentityLte
 import android.telephony.CellInfoLte
-import android.telephony.ClosedSubscriberGroupInfo
-import androidx.annotation.RequiresApi
 import com.github.kyuubiran.ezxhelper.utils.findField
 import fuck.location.xposed.cellar.identity.Lte
-import fuck.location.xposed.helpers.ConfigGateway
-import org.lsposed.hiddenapibypass.HiddenApiBypass
 
 class Lte {
     @ExperimentalStdlibApi
-    @RequiresApi(Build.VERSION_CODES.R)
     fun constructNewCellInfoLte(existedCellInfoLte: CellInfoLte): CellInfoLte {
         val existedResultField = findField(existedCellInfoLte.javaClass) {
             name == "mCellIdentityLte"
