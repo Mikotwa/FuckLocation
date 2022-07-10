53843edd3334c24cd8ca2e2b75106afff13d9840
Mikotwa <mikotwa@pm.me>
2022-07-10 18:15:05 +0800

    Implement mock location support

--- app/src/main/java/io/github/mikotwa/yucklocation/hook/location/LocationHooker.kt
+++ app/src/main/java/io/github/mikotwa/yucklocation/hook/location/LocationHooker.kt
@@ -5,6 +5,8 @@ import com.highcapable.yukihookapi.hook.bean.VariousClass
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.log.loggerD
 import com.highcapable.yukihookapi.hook.type.java.IntType
+import com.highcapable.yukihookapi.hook.type.java.StringType
+
 import io.github.mikotwa.yucklocation.hook.utils.ConfigHelper
 import io.github.mikotwa.yucklocation.hook.utils.LocationHelper
 import io.github.mikotwa.yucklocation.hook.utils.PackageNameHelper
@@ -12,6 +14,10 @@ import io.github.mikotwa.yucklocation.hook.utils.PackageNameHelper
 class LocationHooker : YukiBaseHooker() {
     private val LocationProvider = VariousClass(
         "com.android.server.location.provider.LocationProviderManager"
+    )
+    
+    private val LocationManagerService = VariousClass(
+        "com.android.server.location.LocationManagerService"
     )
 
     private fun lastLocation() {
@@ -30,6 +36,22 @@ class LocationHooker : YukiBaseHooker() {
         
         val CallerIdentityClass = VariousClass(
             "android.location.util.identity.CallerIdentity"
+        )
+
+        val IGnssStatusListener = VariousClass(
+            "android.location.IGnssStatusListener"
+        )
+
+        val IGnssNmeaListener = VariousClass(
+            "android.location.IGnssNmeaListener"
+        )
+
+        val GeofenceClass = VariousClass(
+            "android.location.Geofence"
+        )
+
+        val PendingIntentClass = VariousClass(
+            "android.app.PendingIntent"
         )
 
         LocationProvider.hook {
@@ -71,11 +93,106 @@ class LocationHooker : YukiBaseHooker() {
                 }
             }
         }
+
+        LocationManagerService.hook {
+            injectMember {
+                method {
+                    name = "getLastLocation"
+                    param(StringType, LastLocationRequestClass, StringType, StringType)
+                }
+                afterHook {
+                    val packageName = args[2] as String
+
+                    loggerD(msg = "in getLastLocation (LocationManagerService)! Caller package name: $packageName")
+
+                    if (ConfigHelper.get().isPackageInScope(packageName)) {
+                        loggerD(msg = "$packageName is in scope! Return custom location")
+
+                        result = LocationHelper().generateMockedLocation(result)
+                    }
+                }
+            }
+        }
+
+        LocationManagerService.hook {
+            injectMember {
+                method {
+                    name = "getCurrentLocation"
+                    param(StringType, LocationRequestClass, ILocationCallbackClass, StringType, StringType)
+                }
+                afterHook {
+                    val packageName = args[3] as String
+
+                    loggerD(msg = "in getCurrentLocation (LocationManagerService)! Caller package name: $packageName")
+
+                    if (ConfigHelper.get().isPackageInScope(packageName)) {
+                        loggerD(msg = "$packageName is in scope! Return null")
+
+                        result = null
+                    }
+                }
+            }
+        }
+
+        LocationManagerService.hook {
+            injectMember {
+                method {
+                    name = "registerGnssStatusCallback"
+                    param(IGnssStatusListener, StringType, StringType, StringType)
+                }
+                beforeHook {
+                    val packageName = args[1] as String
+
+                    loggerD(msg = "in registerGnssStatusCallback (LocationManagerService)! Caller package name: $packageName")
+
+                    if (ConfigHelper.get().isPackageInScope(packageName)) {
+                        loggerD(msg = "$packageName is in scope! Dropping register request...")
+                        result = null
+                    }
+                }
+            }
+        }
+
+        LocationManagerService.hook {
+            injectMember {
+                method {
+                    name = "registerGnssNmeaCallback"
+                    param(IGnssNmeaListener, StringType, StringType, StringType)
+                }
+                beforeHook {
+                    val packageName = args[1] as String
+
+                    loggerD(msg = "in registerGnssNmeaCallback (LocationManagerService)! Caller package name: $packageName")
+
+                    if (ConfigHelper.get().isPackageInScope(packageName)) {
+                        loggerD(msg = "$packageName is in scope! Dropping register request...")
+                        result = null
+                    }
+                }
+            }
+        }
+
+        LocationManagerService.hook {
+            injectMember {
+                method {
+                    name = "requestGeofence"
+                    param(GeofenceClass, PendingIntentClass, StringType, StringType)
+                }
+                beforeHook {
+                    val packageName = args[2] as String
+
+                    loggerD(msg = "in requestGeofence (LocationManagerService)! Caller package name: $packageName")
+
+                    if (ConfigHelper.get().isPackageInScope(packageName)) {
+                        loggerD(msg = "$packageName is in scope! Dropping register request...")
+                        result = null
+                    }
+                }
+            }
+        }
     }
 
     override fun onHook() = YukiHookAPI.encase {
-        loggerD(msg = "Hello!")
-
         lastLocation()
     }
 }
