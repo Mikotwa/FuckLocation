c2cb11fe5d98c1aedc47219e327711982ea9da2b
Mikotwa <mikotwa@pm.me>
2022-07-10 15:10:26 +0800

    Reborn in new flame

---
+++ app/src/main/java/io/github/mikotwa/yucklocation/hook/HookEntry.kt
@@ -0,0 +1,21 @@
+package io.github.mikotwa.yucklocation.hook
+
+import com.highcapable.yukihookapi.YukiHookAPI
+import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
+import com.highcapable.yukihookapi.hook.factory.configs
+import com.highcapable.yukihookapi.hook.log.loggerD
+import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
+
+@InjectYukiHookWithXposed
+class HookEntry() : IYukiHookXposedInit{
+    override fun onInit() = configs {
+        debugTag = "YuckLocation"
+        isDebug = true
+    }
+
+    override fun onHook() = YukiHookAPI.encase{
+        loadApp("android") {
+            loggerD(msg = "Hooking android!")
+        }
+    }
+}
